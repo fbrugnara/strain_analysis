00663c30cd4a444915fe25b902d82d8d3a1c9275
fbrugnara <yousp4mme@gmail.com>
2015-12-16 11:03:21 +0100

    Initial add of empty gui functions

---
+++ pos_sg_gui.m
@@ -0,0 +1,7 @@
+function [ output_args ] = pos_sg_gui( input_args )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ wframes_gui.m
@@ -0,0 +1,7 @@
+function [ output_args ] = wframes_gui( input_args )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
