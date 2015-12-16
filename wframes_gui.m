5be79a845d86c9a5e57f4667f2306f8b2f2d7df8
fbrugnara <yousp4mme@gmail.com>
2015-12-16 17:41:20 +0100

    Try to save data with handles

--- wframes_gui.m
+++ wframes_gui.m
@@ -1,11 +1,43 @@
-function wframes_gui( DICpath,DICfilen )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-   
-    f.fig=figure;
-    for i=1:length(DICfilen); 
-        cbh(i) = uicontrol(f.fig,'Style','checkbox','String',DICfilen(i), ...
-                       'Value',0,'Position',[30 20*i 130 20],...
-                        'Callback',{@checkBoxCallback,i,DICfilen});
+   %% function wframes_gui gets DICpath and DICfilen and returns selected frames
+   function [wframes]=wframes_gui( DICpath,DICfilen )
+   %create figure
+   f.fig=figure;
+        %place checkboxes and name them after DICfilen(i)
+        for i=1:length(DICfilen); 
+            f.cbh(i) = uicontrol(f.fig,'Style','checkbox','String',DICfilen(i), ...
+            'Value',1,'Position',[30 20*i 130 20]);
+        end
+        
+        % Place Button to Submit values 
+        f.btn = uicontrol('Style', 'pushbutton', 'String', 'Submit',...
+        'Position', [20 20*(i+1) 130 20],...
+        'Callback',{@pushbuttoncallback,i,DICfilen,f});  
+   h=findobj
+   wframes=h.UserData
+    
+   end
+   %% callback of the submit button
+   function pushbuttoncallback(hObject,event,i,DICfilen,f)
+    
+    %length of the in put cell
+    lenDICfilen=length(DICfilen);
+    
+    %Check the values of the checkboxes 
+    % .Value returns the value of the checkbox zero unchecked
+    %  one checked
+    for k=1:lenDICfilen
+          val(k)=f.cbh(1,k).Value  
     end
+    %find the indices we want to delete (where val is zero)
+    indices =find(val(1,:)==0)
+    %if the is no entry we want to delete (indices is empty)
+    if isempty(indices) == 0
+    DICfilen(:,indices)=[]
+    else
+       DICfilen=DICfilen
+    end
+    hObject.UserData=DICfilen
+    close(f.fig);
+   end
+   
    
