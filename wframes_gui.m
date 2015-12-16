58ab3fe42c8de8bc222a319b350c890ae2c8732b
fbrugnara <yousp4mme@gmail.com>
2015-12-16 17:17:22 +0100

    Add comments to wframes.gui

--- wframes_gui.m
+++ wframes_gui.m
@@ -1,7 +1,11 @@
-function [ output_args ] = wframes_gui( input_args )
+function wframes_gui( DICpath,DICfilen )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
-
-end
-
+   
+    f.fig=figure;
+    for i=1:length(DICfilen); 
+        cbh(i) = uicontrol(f.fig,'Style','checkbox','String',DICfilen(i), ...
+                       'Value',0,'Position',[30 20*i 130 20],...
+                        'Callback',{@checkBoxCallback,i,DICfilen});
+    end
+   
