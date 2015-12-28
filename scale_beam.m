7f911f599962b699be90f66fea16dbc7e8c1e7e1
fbrugnara <yousp4mme@gmail.com>
2015-12-28 04:06:11 +0100

    preallocated arrays and added user instructions

--- scale_beam.m
+++ scale_beam.m
@@ -1,21 +1,27 @@
 function [ scale ] = scale_beam( beam_image,height_beam )
 %SCALE_BEAM Summary of this function goes here
 %   Detailed explanation goes here
-f = figure()
-ax=gca
+f = figure();
+f.MenuBar='none';
+ax=gca;
 lines=10;
-img_beam=imshow(beam_image,'Parent',ax)
+
+imshow(beam_image,'Parent',ax);
+title(ax,'Place ends of distance tool on edge of object and press enter. (repeats 10 times)')
 xlim=get(gca,'Xlim');
 distx=xlim(2)-xlim(1);
 intx=distx/(lines+1);
 
 ylim=get(gca,'Ylim');
 %disty=ylim(2)-ylim(1);
-y_upperb=0.7*ylim(2)
-y_lowerb=0.3*ylim(2)
+y_upperb=0.7*ylim(2);
+y_lowerb=0.3*ylim(2);
+%img_px_measure=zeros(lines,1);
+pixel_length=zeros(lines,1);
+%img_px_measure(1:lines)=imdistline();
 for i=1:lines
-img_px_measure(i)=imdistline
-setPosition(img_px_measure(i),[intx*i y_lowerb;intx*i y_upperb])
+img_px_measure(i)=imdistline(ax);
+setPosition(img_px_measure(i),[intx*i y_lowerb;intx*i y_upperb]);
 api = iptgetapi(img_px_measure(i));
 fcn = makeConstrainToRectFcn('imline',[intx*i intx*i],...
    get(gca,'YLim'));
@@ -25,8 +31,7 @@ pause()
 pixel_length(i)=api.getDistance();
 end
 close(f);
-pixel_length;
 mean_pixel_length=mean(pixel_length);
 scale=mean_pixel_length/height_beam;
+ 
 end
-
