c3fdeded3b10a4d422f3016c6240645eb34325f8
Florian Brugnara <yousp4mme@gmail.com>
2016-01-07 18:17:14 +0100

    modifie references to cells/structs

--- fieldextraction2.m
+++ fieldextraction2.m
@@ -1,11 +1,17 @@
 function [ extracted_data ] = fieldextraction2(loadedframes,wframes_wo_ext,wfields,strgau_center,radius)
 %FIELDEXTRACTION2 Summary of this function goes here
 %   Detailed explanation goes here
+% Passt für eyy if vertical dms 
+% width_half=ceil(radius{2})/2;
+%     length_half=ceil(radius{3})/2;
+%     x=strgau_center(1,1);
+%     y=strgau_center(1,2);
 
-width_half=ceil(radius{2})/2;
-    length_half=ceil(radius{3})/2;
-    x=strgau_center(1,2);
-    y=strgau_center(1,1);
+% Passt für exx if horizontal dms
+width_half=ceil(radius{3})/2;
+    length_half=ceil(radius{2})/2;
+    x=strgau_center(1,1);
+    y=strgau_center(1,2);
     if radius{1,1}=='r'
 for i=1:length(wframes_wo_ext)
     for j=1:length(wfields)
